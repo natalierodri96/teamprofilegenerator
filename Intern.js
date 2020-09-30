93e60b5e1485eabe3f2795955191023b2592d280
Natalie Rodriguez <natalierodri96@gmail.com>
2020-09-30 18:35:22 -0500

    6th commit

--- Intern.js
+++ Intern.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+class Intern extends Employee {
+    constructor(name, email, id, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+        getRole(){ 
+            return "intern";
+        }
+        get(){
+            return this.school
+        }
+
+}
+module.exports = Intern
